65f31b78a0a807094b013431d16c51b3c1b6f478
amjad isleem <amjadesleem@gmail.com>
2023-10-03 10:37:09 +0300

    Add unittest File

--- src/test/java/AssertTest.java
+++ src/test/java/AssertTest.java
@@ -1,6 +1,20 @@
 import org.testng.Assert;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 public class AssertTest {
+    @BeforeMethod
+    public void printBStatus(ITestResult result){
+        System.out.println(result.getMethod().getMethodName()+"is Started");
+    }
+
+    @AfterMethod
+    public void printAStatus(ITestResult result){
+        System.out.println(result.getMethod().getMethodName()+"is Finish");
+    }
+
 
     @Test(priority=1)
     public void testMultiplication(){
